fb3edc1af17d5a30eebf45890ab3260185cc160b
adiel <adiel.bm5@gmail.com>
2021-11-15 18:28:38 +0200

    client apollo

---
+++ apollo/client.js
@@ -0,0 +1,10 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { API_URL } from "../utils/api";
+
+const client = new ApolloClient({
+    uri: `${API_URL}/graphql`,
+    cache: new InMemoryCache(),
+});
+
+
+export default client;
