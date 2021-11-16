c2549c0004dc75f02f343f84bd7a05fce3cc12cd
adiel <adiel.bm5@gmail.com>
2021-11-16 17:53:29 +0200

    add meta viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import Header from '../components/Header'
 import '../styles/index.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header/>
       <Component {...pageProps} />
     </div>
