d60ef4f43417e1e766dbab6a7b7bfae0e8043dc0
adiel <adiel.bm5@gmail.com>
2021-11-16 17:55:35 +0200

    prettier fix

--- utils/fromImageToUrl.js
+++ utils/fromImageToUrl.js
@@ -2,13 +2,13 @@ import { API_URL } from './api'
 
 export const fromImageToUrl = (image) => {
 
-    if (!image) {
-      return "/vercel.svg"; //Or default image here
-    }
-    if (image.url.indexOf("/") === 0) {
-      //It's a relative url, add API URL
-      return `${API_URL}${image.url}`;
-    }
-  
-    return image.url;
+  if (!image) {
+    return "/vercel.svg"; //Or default image here
+  }
+  if (image.url.indexOf("/") === 0) {
+    //It's a relative url, add API URL
+    return `${API_URL}${image.url}`;
+  }
+
+  return image.url;
 };
