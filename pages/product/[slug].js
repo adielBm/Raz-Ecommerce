f2050042ac36aa356fe56233de6671989e4f422c
adiel <adiel.bm5@gmail.com>
2021-11-16 17:54:34 +0200

    mui Grid & Image

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,26 +2,37 @@ import { fromImageToUrl } from "../../utils/fromImageToUrl"
 import Link from 'next/link'
 import { PRODUCT_BY_SLUG } from "../../apollo/queries"
 import client from "../../apollo/client"
+import { Button, Grid } from "@mui/material"
+import Image from 'next/image'
 
 const Product = ({ data }) => {
   const product = data.products[0]
   return (
     <div className="review-card">
-      <img src={fromImageToUrl(product.image)} />
-      <h1>{product.title}</h1>
-      {product.product_categories.map((c) => (
-        <Link key={c.id} href={`/product-category/${c.slug}`}><a><small>{c.title}</small></a></Link>
-      ))}
-      <p>{product.content}</p>
-      <h1>{`${product.price} $`}</h1>
+      <Grid container spacing={5}>
+        <Grid item md={6}>
+          <div>
+            <Image width={600} height={600} src={fromImageToUrl(product.image)} />
+          </div>
+        </Grid>
+        <Grid item md={6}>
+          <h1>{product.title}</h1>
+          {product.product_categories.map((c) => (
+            <Link key={c.id} href={`/product-category/${c.slug}`}><a><small>{c.title}</small></a></Link>
+          ))}
+          <p>{product.content}</p>
+          <h2>{`${product.price} $`}</h2>
+          <Button variant="contained" color="primary">Add To Cart</Button>
+        </Grid>
+      </Grid>
     </div>
   )
-} 
+}
 
 export async function getServerSideProps({ params }) {
   const { data } = await client.query({
     query: PRODUCT_BY_SLUG,
-    variables: {slug: params.slug}
+    variables: { slug: params.slug }
   })
   return { props: { data } }
 }
