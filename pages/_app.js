405ff62b3750043170d496af005b4318462b9db6
adiel <adiel.bm5@gmail.com>
2021-11-19 12:54:38 +0200

    mui theme & cssbaseline

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,27 @@
 import Header from '../components/Header'
-import '../styles/index.css'
 import Head from 'next/head'
 import Footer from '../components/Footer'
+import { ThemeProvider } from '@mui/material'
+import { theme } from '../styles/theme'
+import CssBaseline from '@mui/material/CssBaseline';
+import '../styles/global.css'
+import Container from '@mui/material/Container';
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       </Head>
       <Header />
-      <Component {...pageProps} />
+      <Container maxWidth="xl">
+        <Component {...pageProps} />
+      </Container>
       <Footer />
-    </div>
+    </ThemeProvider>
   )
 }
 
