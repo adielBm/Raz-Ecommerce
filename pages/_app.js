40515b11264a5c0f4f00584466dac1504841ad87
adiel <adiel.bm5@gmail.com>
2021-11-21 01:18:41 +0200

    wrap app in CartContext Provider

--- pages/_app.js
+++ pages/_app.js
@@ -6,21 +6,23 @@ import { theme } from '../styles/theme'
 import CssBaseline from '@mui/material/CssBaseline';
 import '../styles/global.css'
 import Container from '@mui/material/Container';
-
+import CartContextProvider from '../contexts/CartContext'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      </Head>
-      <Header />
-      <Container maxWidth="xl">
-        <Component {...pageProps} />
-      </Container>
-      <Footer />
+      <CartContextProvider>
+        <CssBaseline />
+        <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        </Head>
+        <Header />
+        <Container maxWidth="xl">
+          <Component {...pageProps} />
+        </Container>
+        <Footer />
+      </CartContextProvider>
     </ThemeProvider>
   )
 }
