2791c7c4231ff7be154de080036322c917a4bfbd
adiel <adiel.bm5@gmail.com>
2021-11-23 12:16:32 +0200

    hide header on scroll

--- components/Header.js
+++ components/Header.js
@@ -7,40 +7,54 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import styles from './Header.module.css'
-import Badge from '@mui/material/Badge';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import { CartContext } from "../contexts/CartContext";
-import { useContext } from "react";
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
+import { useCart } from "../hooks/useCart";
+import IconCart from "./IconCart";
+ 
 
-const Header = () => {
-  const cart = useContext(CartContext);
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="relative" >
-        <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href={`/`} >
-              <a>
-                <Typography variant="h4" className={styles.title} >raz ecommerce</Typography>
-              </a>
-            </Link>
-          </Typography>
-          <Box color="white" sx={{ display: { xs: 'flex' } }}>
-            <Link href="/cart">
-              <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                <Badge badgeContent={cart.itemCount} color="secondary">
-                  <ShoppingBasketIcon />
-                </Badge>
-              </IconButton>
-            </Link>
-          </Box>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+const Header = (props) => {
+
+  return (
+    <>
+      <HideOnScroll {...props}>
+        <AppBar  >
+          <Toolbar>
+            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link href={`/`} >
+                <a>
+                  <Typography variant="h4" className={styles.title} >raz ecommerce</Typography>
+                </a>
+              </Link>
+            </Typography>
+            <Box color="white" sx={{ display: { xs: 'flex' } }}>
+              <IconCart/>
+            </Box>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+    </>
   )
 }
 
