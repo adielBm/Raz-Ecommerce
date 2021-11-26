6c57d96c64f2261655778a4c95f2ce5d2b18f276
adiel <adiel.bm5@gmail.com>
2021-11-26 09:05:48 +0200

    product list restyleing

--- components/ProductList.js
+++ components/ProductList.js
@@ -2,36 +2,45 @@ import { fromImageToUrl } from '../utils/fromImageToUrl'
 import Link from 'next/link'
 import Image from 'next/image'
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-
+import CardMedia from '@mui/material/CardMedia';
+import { ButtonBase } from '@mui/material';
 
 const ProductList = (products) => {
+
   return (
     <Grid container spacing={4}>
       {products.products.map((product) => (
-        <Grid key={product.id} item xs={6} md={3}>
-          <Link href={`/product/${product.slug}`}>
-            <a>
-              <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                  <Image width={200} height={200} src={fromImageToUrl(product.image)} />
-                  <h2>{product.title}</h2>
-                  <Typography>{product.content.substring(0, 200)}...</Typography>
-                  <Button variant="contained">Buy</Button>
+        <Grid key={product.id} item md={4}>
+          <Card style={{ height: '200px' }} sx={{ display: 'flex' }} >
+            <Link href={`/product/${product.slug}`}>
+              <ButtonBase >
+                <CardContent style={{ textAlign: 'left' }}>
+                  <Typography component="div" variant="h5">
+                    {product.title}
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary" component="div">
+                    {product.content.substring(0, 50)}...
+                  </Typography>
                 </CardContent>
-              </Card>
-            </a>
-          </Link>
+                <CardMedia title="Your title">
+                  <div style={{ width: '200px', height: '200px', position: 'relative' }}>
+                    <Image
+                      alt='Mountains'
+                      src={fromImageToUrl(product.image)}
+                      width={2}
+                      height={2}
+                      layout="responsive"
+                    />
+                  </div>
+                </CardMedia>
+              </ButtonBase>
+            </Link>
+          </Card>
         </Grid>
-      ))
-      }
+      ))}
     </Grid>
   )
 }
