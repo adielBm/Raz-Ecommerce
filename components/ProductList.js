0e5af49ee50c95ef99c593c7454738ab6dd55fd0
adiel <adiel.bm5@gmail.com>
2021-11-26 13:08:25 +0200

    some changhes from mui to sass

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,47 +1,29 @@
 import { fromImageToUrl } from '../utils/fromImageToUrl'
 import Link from 'next/link'
 import Image from 'next/image'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import CardMedia from '@mui/material/CardMedia';
-import { ButtonBase } from '@mui/material';
+import styles from './ProductList.module.scss'
 
 const ProductList = (products) => {
 
   return (
-    <Grid container spacing={4}>
+    <div className="grid">
       {products.products.map((product) => (
-        <Grid key={product.id} item md={4}>
-          <Card style={{ height: '200px' }} sx={{ display: 'flex' }} >
-            <Link href={`/product/${product.slug}`}>
-              <ButtonBase >
-                <CardContent style={{ textAlign: 'left' }}>
-                  <Typography component="div" variant="h5">
-                    {product.title}
-                  </Typography>
-                  <Typography variant="subtitle1" color="text.secondary" component="div">
-                    {product.content.substring(0, 50)}...
-                  </Typography>
-                </CardContent>
-                <CardMedia title="Your title">
-                  <div style={{ width: '200px', height: '200px', position: 'relative' }}>
-                    <Image
-                      alt='Mountains'
-                      src={fromImageToUrl(product.image)}
-                      width={2}
-                      height={2}
-                      layout="responsive"
-                    />
-                  </div>
-                </CardMedia>
-              </ButtonBase>
-            </Link>
-          </Card>
-        </Grid>
+        <div className={`${styles.product} block`} key={product.id}>
+          <Link href={`/product/${product.slug}`}>
+            <a className="flex">
+              <img
+                className={styles.image}
+                src={fromImageToUrl(product.image)}
+              />
+              <div>
+                <h3>{product.title}</h3>
+                <span>{product.content.substring(0, 30)}...</span>
+              </div>
+            </a>
+          </Link>
+        </div>
       ))}
-    </Grid>
+    </div>
   )
 }
 
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,7 +2,7 @@ import { fromImageToUrl } from "../../utils/fromImageToUrl"
 import Link from 'next/link'
 import { PRODUCT_BY_SLUG } from "../../apollo/queries"
 import client from "../../apollo/client"
-import { Button, Grid, Typography } from "@mui/material"
+import { Grid } from "@mui/material"
 import Image from 'next/image'
 import { useCart } from "../../hooks/useCart"
 
@@ -18,13 +18,13 @@ const Product = ({ data }) => {
         </div>
       </Grid>
       <Grid item md={6}>
-        <Typography variant="h1" color="initial">{product.title}</Typography>
+        <h1>{product.title}</h1>
         {product.product_categories.map((c) => (
           <Link key={c.id} href={`/product-category/${c.slug}`}><a><small>{c.title}</small></a></Link>
         ))}
-        <Typography>{product.content}</Typography>
-        <Typography variant="h4" component="div">{`${product.price} $`}</Typography>
-        <Button variant="contained" color="primary" onClick={() => addProduct(product)}>Add To Cart</Button>
+        <div>{product.content}</div>
+        <h2>{`${product.price} $`}</h2>
+        <button onClick={() => addProduct(product)}>Add To Cart</button>
       </Grid>
     </Grid>
   )
