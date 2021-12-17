4b87de7f939d9bfdefbf0c255d93a4af25c64f0e
adiel <adiel.bm5@gmail.com>
2021-12-17 14:11:38 +0200

    ProductList css box

--- components/ProductList.js
+++ components/ProductList.js
@@ -7,7 +7,7 @@ const ProductList = ({ products }) => {
     <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:grid-cols-4 xl:gap-x-8">
       {products.map((product) => (
         <Link key={product.attributes.slug} href={`/product/${product.attributes.slug}`} className="group">
-          <a className="bg-white rounded-lg overflow-hidden">
+          <a className="box">
             <div className="h-64 w-auto relative">
               <Image
                 src={fromImageToUrl(product.attributes.image)}
@@ -16,8 +16,10 @@ const ProductList = ({ products }) => {
                 objectFit="cover" // change to suit your needs
               />
             </div>
-            <h3 className="mt-4 text-gray-700">{product.attributes.title}</h3>
-            <p className="mt-1 font-medium text-gray-900">{product.attributes.price}</p>
+            <div className="p-3">
+              <h3 className="text-gray-700">{product.attributes.title}</h3>
+              <p className="font-medium text-gray-900">{product.attributes.price}</p>
+            </div>
           </a>
         </Link>
       ))}
