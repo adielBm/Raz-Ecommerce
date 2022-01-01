ab510260767bf3beb7158ab855020b4e1ce32bfb
adiel <adiel.bm5@gmail.com>
2022-01-01 19:46:11 +0200

    Nav tmp

--- components/Nav/Item.js
+++ components/Nav/Item.js
@@ -1,19 +1,24 @@
 import React, { useState } from 'react'
 import { CSSTransition } from 'react-transition-group'
+import Link from "next/link"
 
-export  default function Item() {
+export default function Item({ data: { attributes } }) {
 
   const [showMessage, setShowMessage] = useState(false);
+
+  if (attributes.subcategories.data.length < 1) return null
 
   return (
     <div
       onMouseEnter={() => setShowMessage(true)}
       onMouseLeave={() => setShowMessage(false)}
     >
-      <div className={` ${showMessage ? `bg-white text-blue-500` : `bg-blue-300 text-white`} 
+      <Link href={`/product-category/${attributes.slug}/`} >
+        <a className={` ${showMessage ? `bg-white text-blue-500` : `bg-blue-300 text-white`} 
           transition ease-in-out cursor-pointer hover: rounded-t-lg h-8 flex items-center px-4`}>
-        Cloting 
-      </div>
+          {attributes.title}
+        </a>
+      </Link>
       <CSSTransition
         in={showMessage}
         timeout={300}
@@ -23,13 +28,16 @@ export  default function Item() {
         <div
           className="absolute left-0 z-10 w-1/2"
           variant="primary"
-          dismissible
           onClose={() => setShowMessage(false)}
         >
-          <div className="bg-white shadow-md p-6 grid gap-4 rounded-b-lg">
-            <div>Cloting A</div>
-            <div>Cloting B</div>
-            <div>Cloting C</div>
+          <div className="bg-white shadow-md p-6 grid grid-cols-2 gap-4 rounded-b-lg">
+            {attributes.subcategories.data.map(({ attributes }) => (
+              <Link key={attributes.slug} href={`/product-category/${attributes.slug}/`}>
+                <a className="p-2 rounded-lg hover:bg-blue-100 transition-all ">
+                  {attributes.title}
+                </a>
+              </Link>
+            ))}
           </div>
         </div>
       </CSSTransition>
--- hooks/getYourStuff.js
+++ hooks/getYourStuff.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
 export default function getYourStuff() {
-  return 'some..'
+
+
+  return [
+    {
+      title: 'Cloting',
+      cats: ['Cloting 🐜', 'Cloting 🙉', 'Cloting 🐳', ]
+    },
+    {
+      title: 'Dne',
+      cats: ['Pom 😀', 'Eolc 🦝', 'Mose 👿', ]
+    }
+  ]
 }
