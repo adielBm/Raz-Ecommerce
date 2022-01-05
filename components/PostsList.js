d9e9153403a4956bb2fb23b934793dd0a0be9d51
adiel <adiel.bm5@gmail.com>
2022-01-05 16:11:01 +0200

    post page

---
+++ components/PostsList.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { getStrapiMedia } from '../utils'
+import { truncate } from '../utils';
+const removeMd = require('remove-markdown');
+
+const PostsList = ({ posts }) => {
+  return (
+    <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:grid-cols-4 xl:gap-x-8">
+      {posts.map((post) => (
+        <Link key={post.attributes.slug} href={`/blog/${post.attributes.slug}`} className="group">
+          <a className="box">
+            <div className="h-64 w-auto relative">
+              <Image
+                src={getStrapiMedia(post.attributes.image.data)}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <div className="p-3">
+              <h3 className="text-gray-700">{post.attributes.title}</h3>
+              <p className="text-gray-700">{ truncate(removeMd(post.attributes.content), 38)  }</p>
+            </div>
+          </a>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default PostsList
---
+++ pages/blog/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+import PostsList from '../../components/PostsList'
+import client from '../../apollo/client'
+import { POSTS } from '../../apollo/queries'
+
+const Blog = ({ data }) => {
+
+  return (
+    <div>
+      <h1>Blog Lock</h1>
+      <PostsList posts={data.posts.data} />
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const { data } = await client.query({
+    query: POSTS
+  })
+  return { props: { data } }
+}
+export default Blog
