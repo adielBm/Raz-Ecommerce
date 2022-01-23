3afa60a4dafd3823a7a9220fdb1362025e4a3de7
adiel <adiel.bm5@gmail.com>
2022-01-23 21:56:05 +0200

    slider & md

---
+++ components/Slider.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import Image from 'next/image'
+import { getStrapiMedia } from '../utils';
+
+// Swiper
+import SwiperCore, { FreeMode, Navigation, Thumbs } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode"
+import "swiper/css/navigation"
+import "swiper/css/thumbs"
+
+SwiperCore.use([FreeMode, Navigation, Thumbs]);
+
+export default function Slider({image, gallery}) {
+
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  const getSlieds = (height) => {
+    return (
+      <>
+        {getSlide(image.data, height, true)}
+        {gallery.data.map(image => getSlide(image, height))}
+      </>
+    )
+  }
+  const getSlide = (img, height, priority = false) => {
+    return (
+      <SwiperSlide key={img.id} >
+        <div className={`${height} w-full relative`}>
+          <Image
+            /* width={600}
+            height={600} */
+            layout="fill"
+            objectFit="cover"
+            src={getStrapiMedia(img)}
+            priority={priority}
+          />
+        </div>
+      </SwiperSlide>
+    )
+  }
+
+
+  return (
+    <div className="mb-8 rounded-lg overflow-hidden shadow-lg border-blue-300 border-2 cursor-pointer">
+      <Swiper
+        className="w-full"
+        style={{ '--swiper-navigation-color': '#fff', '--swiper-pagination-color': '#fff' }}
+        /* spaceBetween={10} */
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+      >
+        {getSlieds('h-96')}
+      </Swiper>
+      {gallery.data.length > 0 &&
+        <Swiper
+          className="w-full "
+          style={{ 'swiper-slide-thumb-active': 'border 3px solid' }}
+          onSwiper={setThumbsSwiper}
+          /* spaceBetween={10} */
+          slidesPerView={gallery.data.length + 1}
+          freeMode={true}
+          watchSlidesProgress={true}
+        >
+          {getSlieds('h-24')}
+        </Swiper>
+      }
+    </div>
+  );
+}
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,109 +4,54 @@ import Image from 'next/image'
 import { getProductBySlug } from "../../apollo/getQueries"
 import { useContext, useState } from "react"
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/free-mode"
-import "swiper/css/navigation"
-import "swiper/css/thumbs"
-
-// import Swiper core and required modules
-import SwiperCore, { FreeMode, Navigation, Thumbs } from 'swiper';
 import { Markdown } from "../../components/Markdown"
 import { CartContext } from "../../contexts/cart/CartContext"
+import Slider from "../../components/Slider"
 
-SwiperCore.use([FreeMode, Navigation, Thumbs]);
 
 const Product = ({ data }) => {
 
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
-
   const id = data.id
   const product = data.product
-  const { addProduct } = useContext(CartContext)
+  const brand = product?.brand?.data?.attributes
 
+  const { addProduct } = useContext(CartContext)
   const [count, setCount] = useState(1)
 
   const handleAddToCart = () => {
-    addProduct({ 
-      product: {  ...product, id }, 
-      count: parseInt(count) 
+    addProduct({
+      product: { ...product, id },
+      count: parseInt(count)
     })
     setCount(1)
   }
 
-  const brand = product?.brand?.data?.attributes
-
-  const getSlide = (img, height, priority = false) => {
-    return (
-      <SwiperSlide key={img.id} >
-        <div className={`${height} w-full relative`}>
-          <Image
-            /* width={600}
-            height={600} */
-            layout="fill"
-            objectFit="cover"
-            src={getStrapiMedia(img)}
-            priority={priority}
-          />
-        </div>
-      </SwiperSlide>
-    )
-  }
-
-  const getSlieds = (height) => {
-    return (
-      <>
-        {getSlide(product.image.data, height, true)}
-        {product.gallery.data.map(image => getSlide(image, height))}
-      </>
-    )
-  }
 
   return (
     <div className="sm:items-start grid-cols-1 grid md:grid-cols-2 gap-12">
 
-      <div >
-        <div className="mb-8 rounded-lg overflow-hidden shadow-lg border-blue-300 border-2 cursor-pointer">
-          <Swiper
-            className="w-full"
-            style={{ '--swiper-navigation-color': '#fff', '--swiper-pagination-color': '#fff' }}
-            /* spaceBetween={10} */
-            navigation={true}
-            thumbs={{ swiper: thumbsSwiper }}
-          >
-            {getSlieds('h-96')}
-          </Swiper>
-          {product.gallery.data.length > 0 &&
-            <Swiper
-              className="w-full "
-              style={{ 'swiper-slide-thumb-active': 'border 3px solid' }}
-              onSwiper={setThumbsSwiper}
-              /* spaceBetween={10} */
-              slidesPerView={product.gallery.data.length + 1}
-              freeMode={true}
-              watchSlidesProgress={true}
-            >
-              {getSlieds('h-24')}
-            </Swiper>
-          }
-        </div>
+      <div>
+        <Slider image={product.image} gallery={product.gallery} />
+
         <div>
 
           <Tabs selectedTabClassName="bg-white" className="w-full">
             <TabList className="flex sm:inline-flex -mb-1  cursor-pointer font-bold overflow-hidden rounded-t-md border-t-4 border-x-4 border-blue-100 gap-1 flex-wrap bg-blue-100 ">
+              <Tab className="p-2 w-40 text-center">Description</Tab>
+              <Tab className="p-2 w-40 text-center">Payment</Tab>
               <Tab className="p-2 w-40 text-center">Shipping</Tab>
-              <Tab className="p-2 w-40 text-center">Pay</Tab>
             </TabList>
             <div className="bg-white p-6 rounded-b-md border-y-4 border-x-4 border-blue-100 ">
               <TabPanel>
-                <h2>Any content 1</h2>
+                <Markdown>
+                  {product.content}
+                </Markdown>
               </TabPanel>
               <TabPanel>
-                <h2>Any content 😀2</h2>
+                <h2>Payment methods 💰 </h2>
+              </TabPanel>
+              <TabPanel>
+                <h2>Any content 🛳</h2>
               </TabPanel>
             </div>
           </Tabs>
@@ -131,22 +76,12 @@ const Product = ({ data }) => {
           <Link key={c.id} href={`/product-category/${c.attributes.slug}`}><a><small>{c.attributes.title}</small></a></Link>
         ))}
 
-        <Markdown>
-          {product.content}
-        </Markdown>
-
         <div className="font-bold text-4xl">{`$ ${product.price}`}</div>
 
         <div className="flex gap-4">
           <button type="submit" onClick={handleAddToCart} className="btn">Add to bag</button>
           <input value={count} onChange={e => setCount(e.target.value)} className="input-text w-20" type="number" placeholder="1" min="1" max="100" />
         </div>
-
-
-
-
-
-
 
       </div>
     </div>
