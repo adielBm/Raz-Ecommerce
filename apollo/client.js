eb75f4f676980ba715db613d49b579abd0a18433
adiel <adiel.bm5@gmail.com>
2022-02-08 11:03:37 +0200

    formating

--- apollo/client.js
+++ apollo/client.js
@@ -1,10 +1,9 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { API_URL } from "../utils/api";
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { API_URL } from '../utils/api'
 
 const client = new ApolloClient({
-    uri: `${API_URL}/graphql`,
-    cache: new InMemoryCache()
-});
+  uri: `${API_URL}/graphql`,
+  cache: new InMemoryCache(),
+})
 
-
-export default client;
+export default client
